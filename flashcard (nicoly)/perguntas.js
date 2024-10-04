2e979f49e0a9b0c4e44e7787785a0a97a31bf88a
nicolyalves05062008 <161833485+nicolyalves05062008@users.noreply.github.com>
2024-10-04 08:16:22 -0300

    Add files via upload

---
+++ flashcard (nicoly)/perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que exeecuta alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é Hi (RAI)'
+)
+
